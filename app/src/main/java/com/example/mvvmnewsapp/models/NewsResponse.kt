8afffbc25db9a90957e1740e017ea25130b37ffd
MikeNikola <mikegoku9@gmail.com>
2022-09-14 22:14:29 +0300

    News App

---
+++ app/src/main/java/com/example/mvvmnewsapp/models/NewsResponse.kt
@@ -0,0 +1,10 @@
+package com.example.mvvmnewsapp.models
+
+
+import com.example.mvvmnewsapp.models.Article
+
+data class NewsResponse(
+    val articles: MutableList<Article>,
+    val status: String,
+    val totalResults: Int
+)
---
+++ app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "f35c038c69294842b00ab8433112e242"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
